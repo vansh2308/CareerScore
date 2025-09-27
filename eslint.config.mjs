113f6b8e883515041f7a515d9fb1b53990dee912
VANSH AGARWAL <vanshagarwal11@gmail.com>
2025-09-27 08:45:40 +0530

    build: Refactor eslint rules

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -12,6 +12,10 @@ const compat = new FlatCompat({
 const eslintConfig = [
   ...compat.extends("next/core-web-vitals", "next/typescript"),
   {
+    rules: {
+      "@typescript-eslint/no-non-null-asserted-optional-chain": "off",
+      "@typescript-eslint/no-explicit-any": "off"
+    },
     ignores: [
       "node_modules/**",
       ".next/**",
